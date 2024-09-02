87cfd94dc201f91c976bef1068e97f45934dc68d
unknown <omartalaat627@gmail.com>
2024-09-02 09:00:44 +0200

    first commit

---
+++ demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //large carpet
+            double large = 35;
+            //small carpet
+            double small = 25;
+            //tax rate
+            double tax = 0.06;
+            //user entering
+            Console.Write("Number of small carpets: ");
+            double userSmall = double.Parse(Console.ReadLine());
+            Console.Write("Number of large carpets: ");
+            double userLarge = double.Parse(Console.ReadLine());
+            double allSmall = small * userSmall;
+            double allLarge = large * userLarge;
+            Console.WriteLine($"Price per small room : ${small}");
+            Console.WriteLine($"Price per small room : ${large}");
+            double cost = allSmall + allLarge;
+            Console.WriteLine($"Cost : ${cost}");
+            double userTax = cost * tax;
+            Console.WriteLine($"Tax :${userTax}");
+            double totalEstimate = cost + userTax;
+            Console.WriteLine($"Total estimate: ${totalEstimate}");
+            Console.WriteLine("This estimate is valide for 30 days");
+        }
+    }
+}
---
+++ search/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int  X =10;
+            int Y = 20;
+            int z = X + Y;
+            Console.WriteLine(z.ToString("C")) ;
+        }
+    }
+}
